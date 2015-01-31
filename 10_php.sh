9bbe58fb053567ee5b809e20b555d89699d74e08
dong <ddliuhb@gmail.com>
2015-02-01 00:28:29 +0800

    php

--- 10_php.sh
+++ 10_php.sh
@@ -1,1 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
+
+# install php and modules
+apt-get install \
+    php5 php5-cli php5-fpm \
+    php5-memcached php5-redis php5-mcrypt \
+    php5-curl
+
+# change default listen settings
+# TODO
--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,7 @@ MAINTAINER Liu Dong <ddliuhb@gmail.com>
 ADD * /build/
 
 # Your custom scripts
+# RUN /build/01_change_mirror.sh
 # RUN /build/01_update.sh
+# RUN /build/10_php.sh
 # RUN /build/99_cleanup.sh
